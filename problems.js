9079c675aa239a88bce5bd30880c3f3c9a2837d1
naymur <naymurmn@gmail.com>
2022-11-16 11:40:49 +0600

    prime number

--- problems.js
+++ problems.js
@@ -27,3 +27,27 @@ function primeNumber(num) {
     }
   }
 }
+
+// another way to calculate prime number
+function isPrimeNumber(num) {
+  // const sqrtNumber = Math.floor(Math.sqrt(num));
+  let prime = num != 1;
+
+  for (let i = 2; i < num; i++) {  //i < sqrtNumber
+    if (num % i == 0) {
+      prime = false;
+      break;
+    }
+  }
+  return prime;
+}
+console.log(isPrimeNumber(1));
+
+// another simple way to calculate prime number
+function isPrimeNumb(n) {
+  for (var i = 2; i < n; i++) { // it will always be less than the parameter so the condition below will never allow parameter to be divisible by itself ex. (7 % 7 = 0) which would return true
+    if (n % i === 0) return false; // when parameter is divisible by i, it's not a prime number so return false
+  }
+  return n > 1; // otherwise it's a prime number so return true (it also must be greater than 1, reason for the n > 1 instead of true)
+}
+console.log(isPrimeNumb(2));
