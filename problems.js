7c0d2d204dfca1623d3561886ce02f656d733da7
naymur <naymurmn@gmail.com>
2022-12-06 13:16:17 +0600

    problem-2

--- problems.js
+++ problems.js
@@ -51,3 +51,54 @@ function isPrimeNumb(n) {
   return n > 1; // otherwise it's a prime number so return true (it also must be greater than 1, reason for the n > 1 instead of true)
 }
 console.log(isPrimeNumb(2));
+
+
+
+// Problem -2 : Get the passengers' names using the data provided 
+// Bonus Part (a)- Return vegetarians/vegans
+// Bonus Part (b)- Sort passengers by the number of connected flights in descending order
+
+const passengers = [
+  {
+    id: 1,
+    passengerName: "Freddie Mercury",
+    isVegetarianOrVegan: false,
+    connectedFlights: 2,
+  },
+  {
+    id: 2,
+    passengerName: "Amy Winehouse",
+    isVegetarianOrVegan: true,
+    connectedFlights: 4,
+  },
+  {
+    id: 3,
+    passengerName: "Kurt Cobain",
+    isVegetarianOrVegan: true,
+    connectedFlights: 3,
+  },
+  {
+    id: 3,
+    passengerName: "Michael Jackson",
+    isVegetarianOrVegan: true,
+    connectedFlights: 1,
+  },
+];
+
+// Main Question: Get the passengers' names using the data provided 
+const passengerName = passengers.map(passenger => passenger.passengerName);
+console.log(passengerName);
+
+
+// Bonus Part (a)- Return vegetarians/vegans
+const vegetarians = passengers.map(passenger => {
+  if (passenger.isVegetarianOrVegan) {
+    return passenger;
+  }
+})
+console.log(vegetarians);
+
+
+// Bonus Part (b)- Sort passengers by the number of connected flights in descending order
+const discendingOrder = vegetarians.sort((passengerA, passengerB) => passengerB.connectedFlights - passengerA.connectedFlights);
+console.log(discendingOrder);
